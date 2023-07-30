18d9fa77cf046b6edae7ea6f21c9796b86264fe8
Afzal Muhammad <afzaalm059@gmail.com>
2023-07-30 19:18:21 +0100

    Update generateAudio.js

--- frontend/src/generateAudio.js
+++ frontend/src/generateAudio.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default async function getAudio() {
-  const apiKey = "befd5e8a8b03e48ea562a90a0c9f9155";
+  const apiKey = "api-key";
   const url =
     "https://api.elevenlabs.io/v1/text-to-speech/AonmwXR2V46c0iCvhQBe?optimize_streaming_latency=0";
   const headers = {
